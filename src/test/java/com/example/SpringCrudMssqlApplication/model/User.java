5b155b2d61b2e0b033c6eed264e2dee20ec9e5f2
Junaid Khalid <junaidkhalid20001@gmail.com>
2025-12-05 17:26:33 +0500

    fix some errors in code

--- src/test/java/com/example/SpringCrudMssqlApplication/model/User.java
+++ src/test/java/com/example/SpringCrudMssqlApplication/model/User.java
@@ -12,7 +12,7 @@ public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    private Long id;
 
     private String name;
     private String password;
@@ -24,11 +24,11 @@ public class User {
         this.password = password;
     }
 
-    public Integer getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(Integer id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
