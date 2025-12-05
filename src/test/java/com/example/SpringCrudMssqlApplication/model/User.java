8865a867a099671cd968188c3dfc07cb466c047f
Junaid Khalid <junaidkhalid20001@gmail.com>
2025-12-05 15:30:13 +0500

    conversion of datatype
    ....

--- src/test/java/com/example/SpringCrudMssqlApplication/model/User.java
+++ src/test/java/com/example/SpringCrudMssqlApplication/model/User.java
@@ -12,7 +12,7 @@ public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Integer id;
 
     private String name;
     private String password;
@@ -24,11 +24,11 @@ public class User {
         this.password = password;
     }
 
-    public Long getId() {
+    public Integer getId() {
         return id;
     }
 
-    public void setId(Long id) {
+    public void setId(Integer id) {
         this.id = id;
     }
 
